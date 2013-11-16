89a1480260e097835058fda929086de9e8e50549
Chris Peck <peckpeckpeck@gmail.com>
2013-11-16 17:01:28 -0500

    added scale degree fill-in exercise

--- exam/my_exam.rb
+++ exam/my_exam.rb
@@ -5,6 +5,7 @@ exam = TheoryExam.new(
 	:forms => 2 #each form of the exam will have different randomized items in each exercise
 )
 
+=begin
 exam.addExercise(
 	:type => "rhythms",
 	:bars => 6,	#number of bars in each item
@@ -22,11 +23,13 @@ exam.addExercise(
 	:scale => "major",
 	:items => 5
 )
+=end
 
 exam.addExercise(
 	:type => "degrees",
 	:items => 10
 )
+=begin
 
 exam.addExercise(
 	:type => "intervals",
@@ -72,5 +75,6 @@ exam.addExercise(
 	:type => "analysis",
 	:name => "winchester" #reads 2 files: winchester.ly and winchester-key.ly
 )
+=end
 
 exam.compile
