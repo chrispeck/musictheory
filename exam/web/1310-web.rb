0d14aeb5618d1e5c7951726a40fcdf489d1f7ae8
Chris Peck <peckpeckpeck@gmail.com>
2013-11-19 10:11:14 -0500

    starting web interface as sinatra app

---
+++ exam/web/1310-web.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+load '1310.rb'
+
+get '/' do #form for user to enter options
+	
+end
+post '/' do #generate exam on post
+	exam = TheoryExam.new(
+		:title => "My Final Exam",
+		:forms => 2 #each form of the exam will have different randomized items in each exercise
+	)
+	exam.addExercise(
+		:type => "rhythms",
+		:bars => 6,	#number of bars in each item
+		:items => 2
+	)
+	exam.compile
+
+	'Exam Generation Complete!'
+end
