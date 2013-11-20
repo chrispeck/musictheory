b738997a230b6bbb50e0ec5143279dd028154211
Chris Peck <peckpeckpeck@gmail.com>
2013-11-19 22:38:02 -0500

    generating exam dynamically based on form input

--- exam/web/1310-web.rb
+++ exam/web/1310-web.rb
@@ -5,18 +5,16 @@ get '/' do #form for user to enter options
 	erb :index
 end
 post '/' do #generate exam on post
-	exam = TheoryExam.new(
-		:title => "my_web_exam",
+
+	@exam = TheoryExam.new(
+		:title => params[:name],
 		:forms => 1 #each form of the exam will have different randomized items in each exercise
 	)
-	exam.addExercise(
-		:type => "rhythms",
-		:bars => 6,	#number of bars in each item
-		:items => 1 
+	@exam.addExercise(
+		:type => params[:type],
+		:items => params[:items].to_i 
 	)
-	exam.compile
+	@exam.compile
 
-
-	'Exam Generation Complete! <a href="' + exam.get_url + '">download (tgz archive)</a>'
-
+	erb :response
 end
